74faef45451dd64c9c22e04783b7c680c8fddbc6
Jason Mc <info@jmc-website-design.co.uk>
2023-07-29 22:04:56 +0100

    favourites, modal, basic styling

--- src/vue/_AppView.vue
+++ src/vue/_AppView.vue
@@ -3,13 +3,24 @@
         data() {
             return {
                 db: null,
+                favourites: [],
                 filteredDb: null,
                 currentFilter: null,
-                vehicleTypes: null
+                vehicleTypes: null,
+                currentVehicle: null
             }
         },
         mounted() {
             this.fetchDB()
+        },
+        watch: {
+            favourites: {
+                handler(arr) {
+                    localStorage.setItem("gta5-fav-vehicles", JSON.stringify(arr))
+                },
+                deep: true
+            }
+
         },
         methods: {
             fetchDB() {
@@ -24,9 +35,20 @@
             getVehicleTypes() {
                 this.vehicleTypes = this.db.map(obj => Object.keys(obj)[0])
             },
+            getVehicleInfo(e) {
+                this.currentVehicle = {...e}
+                this.$refs.vehicleInfo.showModal()
+            },
+            closeModal() {
+                this.$refs.vehicleInfo.close()
+            },
             filter(event) {
                 this.filteredDb = this.db.find(obj => Object.keys(obj)[0] === event.target.value)
                 this.currentFilter = event.target.value
+                const savedfavourites = localStorage.getItem('gta5-fav-vehicles')
+                if(savedfavourites) {
+                    this.favourites = JSON.parse(savedfavourites)
+                }
             }
         }
     }
@@ -39,34 +61,50 @@
             <option v-for="item in vehicleTypes" :key="item" :value="item">
                 {{ item }}
             </option>
-        </select><br>
+        </select>
+        <dialog ref="vehicleInfo" class="vehicleInfo">
+            <button @click="closeModal()">close</button>
+            <div v-if="currentVehicle" class="vehicleInfo__details">
+                <div>
+                    <h2>{{ currentVehicle.Name }}</h2>
+                    <strong>Type:</strong> {{ currentVehicle.Type }}
+                    <strong>Seats:</strong> {{ currentVehicle.Seats }}
+                </div>
+
+                <div>
+                    <h3>Stats (out of 100)</h3>
+                    <strong>Speed:</strong> {{ Math.round(currentVehicle.Speed) }}<br>
+                    <strong>Acceleration:</strong> {{ Math.round(currentVehicle.Acceleration) }}<br>
+                    <strong>Braking:</strong> {{ Math.round(currentVehicle.Braking) }}<br>
+                    <strong>Handling:</strong> {{ Math.round(currentVehicle.Handling) }}
+                </div>
+                
+                <div>
+                    <span v-if="currentVehicle.Personal">Personal</span>
+                    <span v-if="currentVehicle.Premium">Premium</span>
+                    <span v-if="currentVehicle.Moddable">Moddable</span>
+                    <span v-if="currentVehicle.SuperModdable">Super Moddable</span>
+                </div>
+
+                <div v-if="currentVehicle.ForSale">
+                    <strong>Purchaseable at:</strong><br> {{ currentVehicle.Website }}<br>
+                    <strong>Cost:</strong> {{ currentVehicle.Cost }}<br>
+                </div>
+                
+                <div v-if="currentVehicle.Sellable">
+                    Can be sold for <strong>{{ currentVehicle.SellPrice }}</strong>
+                </div>
+            </div>
+        </dialog>
 
         <div class="VehicleList">
             <!-- return filtered list -->
             <div class="Vehicle" v-if="currentFilter" v-for="vehicle in filteredDb[currentFilter]" :key="vehicle">
-                <img class="Vehicle__image" :src="`/images/${vehicle.Url}.jpg`" width="200" height="200" :alt="vehicle.Name">
+                <span v-if="vehicle.Website">BUY</span>
+                <button @click="getVehicleInfo(vehicle)">info</button>
+                <input type="checkbox" :value="vehicle.Url" v-model="favourites" />
+                <img class="Vehicle__image" :src="`images/${vehicle.Url}.jpg`" :alt="vehicle.Name">
                 <h2 class="Vehicle__name">{{ vehicle.Name }}</h2>
-                <!--
-                    vehicle.Type
-                    vehicle.Speed
-                    vehicle.Acceleration
-                    vehicle.Braking
-                    vehicle.Handling
-                    vehicle.TopSpeed
-                    vehicle.TopAcceleration
-                    vehicle.TopBraking
-                    vehicle.TopHandling
-                    vehicle.ForSale
-                    vehicle.Website
-                    vehicle.Cost
-                    vehicle.Seats
-                    vehicle.Personal
-                    vehicle.Premium
-                    vehicle.Moddable
-                    vehicle.SuperModdable
-                    vehicle.Sellable
-                    vehicle.SellPrice
-                -->
             </div>
         </div>
     </div>
